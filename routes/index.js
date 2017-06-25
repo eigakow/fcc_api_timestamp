c2ad487d29e9ce9c84b588b1fc7e7a821f7a220b
eigakow <iga.kowalska@gmail.com>
2017-06-25 15:21:53 +0200

    first prerelease

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
+var moment = require ('moment');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/:time', function(req, res) {
+  var input = req.params.time;
+  var unix = null;
+  var nat = null;
+  if (!isNaN(input)) {
+    console.log("Epoch detected");
+    var unix = input;
+    var nat = epochToString(parseInt(input));
+  }
+  else if (moment(input).isValid()) {
+    console.log("String detected");
+    var unix = stringToEpoch(input);
+    var nat = epochToString(stringToEpoch(input));
+  }
+
+  res.json({ "unix": unix, "natural": nat});
 });
 
 module.exports = router;
+
+
+function epochToString(sec) {
+  var d = moment.unix(sec);
+  return d.format("MMMM D, YYYY");
+}
+
+function stringToEpoch(str) {
+  var d = moment(str);
+  return d.format("X");
+}
